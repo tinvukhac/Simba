c679c1b2d2a5e78667ab7e0398ae806e2c67029a
tinvukhac <tinvukhac@gmail.com>
2017-06-10 19:38:47 -0700

    sample for date conversion

--- src/main/scala/org/apache/spark/sql/simba/examples/Hello.scala
+++ src/main/scala/org/apache/spark/sql/simba/examples/Hello.scala
@@ -1,28 +1,40 @@
 package org.apache.spark.sql.simba.examples
-import org.apache.spark._
-import org.apache.spark.SparkContext._
+
+import org.apache.spark.sql.simba.Dataset
+import org.apache.spark.sql.simba.SimbaSession
+import org.apache.spark.sql.simba.index.RTreeType
+import org.apache.spark.sql.functions.{unix_timestamp, to_date}
+import java.sql.Date
+import java.text.SimpleDateFormat
 
 object Hello {
+  
+  case class Trajectory(trajId: Long, seqId: Long, lon: Double, lat:Double, time: Date)
+  
  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+   val simba = SimbaSession
+      .builder()
+      .master("local[4]")
+      .appName("CS236BuildingRTreeIndex")
+      .config("simba.index.partitions", "64")
+      .getOrCreate()
+      
+    import simba.implicits._  
+    import simba.simbaImplicits._
+    val df = simba.read.option("header", false).csv("datasets/trajectories.csv")
+    val df2 = df.toDF("trajId", "seqId", "lon", "lat", "time")
+    val df3 = df2.filter("lat IS NOT NULL").filter("lon IS NOT NULL") 
+    val df4 = df3.select($"trajId", $"seqId", $"lon", $"lat", to_date(unix_timestamp($"time", "yyyy-MM-dd HH:mm:ss").cast("timestamp")).alias("timestamp"))
+    val ds = df4.map(row => Trajectory(row.getString(0).toLong, row.getString(1).toLong, 
+        row.getString(2).toDouble, row.getString(3).toDouble, row.getDate(4)))
+    ds.printSchema()
+    ds.show()
+//    val ds2 = ds.select(to_date(unix_timestamp($"time", "yyyy-MM-dd hh:MM:ss").cast("timestamp")).alias("timestamp"))
+//    ds2.printSchema()
+//    ds2.show()
+    df4.printSchema()
+    df4.show()
     
-    val inputFile = "myfile.txt";
-      //val outputFile = args(1)
-      val conf = new SparkConf().setMaster("local").setAppName("wordCount")
-      // Create a Scala Spark Context.
-      val sc = new SparkContext(conf)
-      // Load our input data.
-      val input =  sc.textFile(inputFile)
-      // Split up into words.
-      val words = input.flatMap(line => line.split(" "))
-      // Transform into word and count.
-      val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
-      // Save the word count back out to a text file, causing evaluation.
-      //counts.saveAsTextFile(outputFile)
-       counts.collect().foreach(println);
-       sc.stop();
-       
-      
-      
+    simba.stop()
   }
 }
